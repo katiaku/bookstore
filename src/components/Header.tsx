43267f7c1573f1c64383c4f65848089dc61e30cb
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-21 21:00:36 +0200

    Añade primeros componentes

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import Logo from "./Logo";
+
+export default function Header() {
+    return (
+        <Logo />
+    )
+}
---
+++ src/components/Menu.tsx
@@ -0,0 +1,9 @@
+export default function Menu() {
+    return (
+        <nav>
+            <div>Home</div>
+            <div>Libros</div>
+            <div>Log In</div>
+        </nav>
+    )
+}
