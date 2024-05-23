c466087e8767f800441c06ca2c5f05131214871b
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-23 13:14:47 +0200

    Crea HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import BooksPage from './pages/BooksPage'
+// import HomePage from './pages/HomePage'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header />
       <BooksPage />
+      {/* <HomePage /> */}
       <Footer />
     </div>
   )
--- src/pages/BooksPage.tsx
+++ src/pages/BooksPage.tsx
@@ -2,14 +2,13 @@ import BookItem from "../components/BookItem";
 
 export default function BooksPage() {
     return (
-        <div className="bg-blue-950 page-height overflow-y-scroll flex flex-wrap justify-start gap-14 gap-y-20 px-20 pt-20 pb-10">
+        <div className="bg-blue-950 page-height overflow-y-scroll flex flex-wrap justify-start gap-14 gap-y-20 p-20">
             <BookItem />
             <BookItem />
             <BookItem />
             <BookItem />
             <BookItem />
             <BookItem />
-            <div className="bg-blue-950 w-full h-100"></div>
         </div>
     )
 }
