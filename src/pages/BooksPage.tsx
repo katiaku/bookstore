05858a199c8479507f9b0ef273c19d1deaea5fd2
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-23 12:23:03 +0200

    Estiliza el BooksPage

--- src/pages/BooksPage.tsx
+++ src/pages/BooksPage.tsx
@@ -2,6 +2,13 @@ import BookItem from "../components/BookItem";
 
 export default function BooksPage() {
     return (
-        <BookItem />
+        <div className="flex flex-wrap justify-center gap-10 gap-y-20 p-20">
+            <BookItem />
+            <BookItem />
+            <BookItem />
+            <BookItem />
+            <BookItem />
+            <BookItem />
+        </div>
     )
 }
