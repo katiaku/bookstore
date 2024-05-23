e5d6a86335585f07674076351bd7b608523b4e6a
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-23 16:25:55 +0200

    Añade type Book y hace cambios correspondientes

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -1,22 +1,38 @@
 // import styles from './bookItem.module.css';
 import { RiDeleteBin5Fill, RiEdit2Fill } from "react-icons/ri";
+import { Book } from "../config/types";
 
 export default function BookItem() {
+
+    const book1: Book = {
+        id_book: 1,
+        id_user: 1,
+        title: "El Perfume",
+        author: "Patrick Suskind",
+        photo: "https://imagessl2.casadellibro.com/a/l/s7/32/9788432228032.webp",
+        price: 19.9,
+        type: "Tapa dura"
+    }
+
     return (
         <div className='font-poppins w-[400px] h-[280px] relative shadow-md bg-slate-100'>
-            <div className='absolute shadow-md left-[15px] -top-[20px]'>
-                <img src="../../public/img/portada-el-perfume.jpg" alt="Portada del libro El Perfume" width="170" />
+            <div className='absolute shadow-md left-[15px] -top-[20px] h-[280px] overflow-hidden'>
+                <img
+                    src={ book1.photo }
+                    alt={`Foto del libro ${ book1.title } de ${ book1.author }` }
+                    width="170"
+                />
             </div>
 
             <div className='absolute left-[200px] w-1/2 h-full py-[15px] pr-[15px] flex flex-col justify-between'>
                 <div>
-                    <p className='font-bold text-3xl text-slate-700'>El Perfume</p>
-                    <p className='mt-1 mb-10 text-slate-700'>Patrick Suskind</p>
-                    <p className='text-sm italic text-slate-500'>Novela</p>
+                    <p className='font-bold text-3xl text-slate-700'>{ book1.title }</p>
+                    <p className='mt-1 mb-10 text-slate-700'>{ book1.author }</p>
+                    <p className='text-sm italic text-slate-500'>{ book1.type }</p>
                 </div>
 
                 <div className='absolute -left-[37px] -bottom-[15px] w-[60px] h-[60px] rounded-full bg-orange-400 text-white flex justify-center items-center shadow-md'>
-                    <span>19.9€</span>
+                    <span>{ book1.price }€</span>
                 </div>
 
                 <div className='self-end flex gap-2'>
---
+++ src/config/types.ts
@@ -0,0 +1,11 @@
+type Book = {
+    id_book: number;
+    id_user: number;
+    title: string;
+    author: string;
+    photo: string;
+    price: number;
+    type: string;
+};
+
+export type { Book };
