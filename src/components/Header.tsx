30f749a8eef606bf6c416660fb9b3f5b3a4fc29f
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-24 16:51:10 +0200

    Añade funcionalidad para abrir y cerrar el Sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,22 @@
 import { GiHamburgerMenu } from "react-icons/gi"; 
 import Logo from "./Logo";
 import Menu from "./Menu";
+import { useState } from "react";
 
 export default function Header() {
+
+    const [isOpenSidebar, setIsOpenSidebar] = useState(false);
+
     return (
         <div className="flex items-center px-4 md:px-10 h-[90px] bg-white">
             <Logo />
             <Menu />
-            <button className="md:hidden font-bold text-4xl text-blue-950 ml-auto mr-2"><GiHamburgerMenu /></button>
+            <button
+                className="md:hidden font-bold text-4xl text-blue-950 ml-auto mr-2"
+                onClick={() => setIsOpenSidebar(!isOpenSidebar)}
+            >
+                <GiHamburgerMenu />
+            </button>
         </div>
     )
 }
