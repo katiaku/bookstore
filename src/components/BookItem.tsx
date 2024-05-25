d9e70fc882ac055a2b842a9a6c25b32b3e5fd817
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-25 13:02:39 +0200

    Añade responsiveness al BookItem

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -1,4 +1,3 @@
-// import styles from './bookItem.module.css';
 import { RiDeleteBin5Fill, RiEdit2Fill } from "react-icons/ri";
 import { BookItemProps } from "../config/types";
 
@@ -15,7 +14,7 @@ export default function BookItem(props: BookItemProps) {
     }
 
     return (
-        <div className='font-poppins w-[400px] h-[280px] relative shadow-md bg-slate-100'>
+        <div className='font-poppins w-full md:w-[400px] h-[280px] relative shadow-md bg-slate-100'>
             <div className='absolute shadow-md left-[15px] -top-[20px] max-h-[280px] overflow-hidden'>
                 <img
                     src={ book.photo }
@@ -24,14 +23,14 @@ export default function BookItem(props: BookItemProps) {
                 />
             </div>
 
-            <div className='absolute left-[200px] w-1/2 h-full py-[15px] pr-[15px] flex flex-col justify-between'>
+            <div className='bg-slate-100/70 absolute right-0 md:left-[200px] w-1/2 h-full py-[15px] px-[15px] md:pr-[15px] md:pl-0 flex flex-col justify-between items-start'>
                 <div>
-                    <p className='font-bold text-3xl text-slate-700'>{ book.title }</p>
+                    <p className='font-bold text-2xl md:text-3xl text-slate-700'>{ book.title }</p>
                     <p className='mt-1 mb-10 text-slate-700'>{ book.author }</p>
-                    <p className='text-sm italic text-slate-500'>{ book.type }</p>
+                    <p className='text-xs md:text-sm italic text-slate-500'>{ book.type }</p>
                 </div>
 
-                <div className='absolute -left-[37px] -bottom-[15px] w-[60px] h-[60px] rounded-full bg-orange-400 text-white flex justify-center items-center shadow-md'>
+                <div className='absolute -left-[20%] md:-left-[37px] -bottom-[15px] w-[60px] h-[60px] rounded-full bg-orange-400 text-white flex justify-center items-center shadow-md'>
                     <span>{ book.price }€</span>
                 </div>
 
