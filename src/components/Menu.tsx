612f21995559b36ad85ee0a2bdd9a6bfdb4cc524
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-25 13:13:22 +0200

    Cambia los estilos del Menu en modo movil

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,22 +1,22 @@
 export default function Menu() {
 
     return (
-        <nav className="flex flex-col md:flex-row w-1/2 h-full mx-auto justify-start md:justify-center items-start md:items-center font-bold uppercase font-poppins text-xl md:text-base gap-8 md:gap-0">
+        <nav className="flex flex-col md:flex-row md:w-1/2 h-full mx-auto justify-center items-center font-bold uppercase font-poppins text-xl md:text-base gap-0">
             <a
                 href="#"
-                className="md:h-full md:px-16 lg:px-20 flex items-center justify-center text-blue-950 transition-all ease-in-out duration-300 md:hover:bg-orange-400 hover:text-white"
+                className="w-full h-full md:px-16 lg:px-20 flex items-center justify-center text-blue-950 transition-all ease-in-out duration-300 hover:bg-blue-950 md:hover:bg-orange-400 hover:text-white"
             >
                 Home
             </a>
             <a
                 href="#"
-                className="md:h-full md:px-16 lg:px-20 flex items-center justify-center text-blue-950 transition-all ease-in-out duration-300 md:hover:bg-orange-400 hover:text-white"
+                className="w-full h-full md:px-16 lg:px-20 flex items-center justify-center text-blue-950 transition-all ease-in-out duration-300 hover:bg-blue-950 md:hover:bg-orange-400 hover:text-white"
             >
                 Libros
             </a>
             <a
                 href="#"
-                className="md:h-full md:px-16 lg:px-20 flex items-center justify-center text-blue-950 transition-all ease-in-out duration-300 md:hover:bg-orange-400 hover:text-white"
+                className="w-full h-full md:px-16 lg:px-20 flex items-center justify-center text-blue-950 transition-all ease-in-out duration-300 hover:bg-blue-950 md:hover:bg-orange-400 hover:text-white"
             >
                 Login
             </a>
