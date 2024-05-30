90b6e7fdc1bdf9b6e139ee608088a83b4144e369
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-30 14:22:33 +0200

    Rediseña el RegisterPage

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -3,11 +3,15 @@ import RegisterForm from "../components/RegisterForm";
 export default function RegisterPage() {
     return (
         <div className="relative bg-blue-950 page-height overflow-y-scroll">
-            <div className="w-full h-full overflow-hidden opacity-25">
+            <div className="w-full h-full overflow-hidden opacity-25 lg:opacity-100">
                 <img className="h-full w-full object-cover" src="../../public/img/bg-5.avif" alt="" />
             </div>
-            <div className="absolute top-0 z-10 w-full h-full flex justify-center items-center">
-                <RegisterForm />
+    
+            <div className="overflow-hidden absolute top-0 z-10 w-full h-full flex justify-center items-center">
+                <div className="lg:w-1/2 lg:h-full"></div>
+                <div className="w-full lg:w-1/2 flex justify-center items-center lg:bg-blue-950 lg:h-full">
+                    <RegisterForm />
+                </div>
             </div>
         </div>
     )
