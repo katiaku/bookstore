e0781c83180f3cae93dff911db745632abef0ede
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-06-01 16:15:11 +0200

    Crea el UserProvider

--- src/config/types.ts
+++ src/config/types.ts
@@ -17,6 +17,7 @@ type SidebarProps = {
 };
 
 type User = {
+    id_user: number,
     firstName: string;
     lastName: string;
     email: string;
---
+++ src/providers/UserProvider.tsx
@@ -0,0 +1,44 @@
+import { createContext, ReactNode, useState } from "react"
+import { User } from "../config/types";
+
+type UserContextType = {
+    user: User | null,
+    login: (user: User) => void
+    logout: () => void
+}
+
+type UserProviderProps = {
+    children: ReactNode
+};
+
+const UserContext = createContext<UserContextType>({} as UserContextType);
+
+function UserProvider(props: UserProviderProps) {
+
+    const { children } = props;
+
+    const [user, setUser] = useState<User | null>(() => {
+        const userLocalStorage = localStorage.getItem('user');
+        return userLocalStorage ? JSON.parse(userLocalStorage) : null;
+    });
+
+    function login(user: User) {
+        setUser(user);
+        localStorage.setItem('user', JSON.stringify(user));
+    }
+
+    function logout() {
+        setUser(user);
+        localStorage.removeItem('user');
+    }
+    
+    return (
+        <UserContext.Provider value={{ user, login, logout }}>
+            { children }
+        </UserContext.Provider>
+    )
+}
+
+export default UserProvider;
+
+export { UserContext };
