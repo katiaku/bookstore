6057800754e6bcee8021f140c82752edd7ff2c57
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-06-03 18:03:49 +0200

    Corrige el estilo de la Home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,35 +1,39 @@
-import { AiOutlineToTop } from "react-icons/ai"; 
-import { BiSearchAlt2 } from "react-icons/bi"; 
-import { BiLibrary } from "react-icons/bi"; 
+import { BiLogInCircle } from "react-icons/bi"; 
+import { BiPencil } from "react-icons/bi"; 
+// import { AiOutlineToTop } from "react-icons/ai"; 
+// import { BiSearchAlt2 } from "react-icons/bi"; 
+// import { BiLibrary } from "react-icons/bi"; 
 
 export default function HomePage() {
     return (
-        <div className="relative bg-blue-950 page-height overflow-y-scroll flex flex-col flex-wrap justify-center items-center">
+        <div className="bg-blue-950 page-height overflow-y-scroll flex flex-col flex-wrap justify-center items-center">
             <div className="flex h-full">
-                <div className="w-1/2 h-full flex flex-col justify-start py-20 gap-20 px-12">
-                    <h1 className="cursor-default text-white text-6xl font-nunito font-bold leading-tight">Best Place To Keep Your <span className="text-orange-400">Favourite</span> Books.</h1>
-                    <div className="flex gap-8 font-poppins">
-                        <button className="bg-orange-400 border-2 px-4 py-[.8rem] w-[9rem] border-orange-400 text-blue-950 font-semibold">Register</button>
-                        <button className="bg-transparent border-2 px-4 py-[.8rem] w-[9rem] border-orange-400 text-orange-400 font-semibold">Log In</button>
+                <div className="lg:w-1/2 h-full flex flex-col justify-center gap-20 p-6 md:px-20 md:py-6">
+                    <h1 className="cursor-default text-slate-200 text-6xl font-poppins font-bold leading-tight">Best Place To Keep Your <span className="text-orange-400">Favourite</span> Books</h1>
+                    
+                    <div className="flex flex-col md:flex-row gap-6 md:gap-8 font-poppins">
+                        <button className="bg-orange-400 border-2 px-4 py-[.8rem] w-full md:w-[10rem] border-orange-400 text-blue-950 font-semibold flex items-center justify-center gap-3">Register<BiPencil /></button>
+                        <button className="bg-transparent border-2 px-4 py-[.8rem] w-full md:w-[10rem] border-orange-400 text-orange-400 font-semibold flex items-center justify-center gap-3">Log In<BiLogInCircle /></button>
                     </div>
+                    
+                    {/* <div className="cursor-default flex w-full justify-center items-start gap-4 md:gap-20 font-poppins mt-auto">
+                        <div className="flex flex-col gap-5 justify-center items-start">
+                            <BiLibrary className="text-3xl text-white" />
+                            <p className="text-white">Create your own library</p>
+                        </div>
+                        <div className="flex flex-col gap-5 justify-center items-start">
+                            <BiSearchAlt2 className="text-3xl text-white" />
+                            <p className="text-white">Discover your best books</p>
+                        </div>
+                        <div className="flex flex-col gap-5 justify-center items-start">
+                            <AiOutlineToTop className="text-3xl text-white" />
+                            <p className="text-white">Increase your knowledge</p>
+                        </div>
+                    </div> */}
                 </div>
-                <div className="h-full w-1/2 overflow-hidden">
-                    <img src="../../public/img/bg-4.avif" alt="" className="h-full w-full object-cover" />
-                </div>
-            </div>
-            
-            <div className="absolute bottom-0 p-8 cursor-default flex bg-orange-400 w-full justify-center gap-20 font-poppins">
-                <div className="flex flex-col gap-4 justify-center items-center">
-                    <BiLibrary className="text-5xl text-blue-950" />
-                    <p className="font-semibold text-blue-950">Create your own library.</p>
-                </div>
-                <div className="flex flex-col gap-4 justify-center items-center">
-                    <BiSearchAlt2 className="text-5xl text-blue-950" />
-                    <p className="font-semibold text-blue-950">Discover your best books.</p>
-                </div>
-                <div className="flex flex-col gap-4 justify-center items-center">
-                    <AiOutlineToTop className="text-5xl text-blue-950" />
-                    <p className="font-semibold text-blue-950">Increase your knowledge.</p>
+                
+                <div className="hidden lg:block h-full w-1/2 overflow-hidden">
+                    <img src="../../public/img/bg-4.avif" alt="Pile of different books." className="h-full w-full object-cover hover:scale-110 transition-all duration-1000" />
                 </div>
             </div>
         </div>
