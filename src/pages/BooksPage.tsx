65beaa64409e04258b5d8358723286c8fbd251ca
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-06-10 12:35:19 +0200

    Ajusta el uso de UserContext

--- src/pages/BooksPage.tsx
+++ src/pages/BooksPage.tsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import BookItem from "../components/BookItem";
 import { Book } from "../config/types";
-// import { UserContext } from "../providers/UserProvider";
+import { UserContext } from "../providers/UserProvider";
 
 export default function BooksPage() {
 
     const [books, setBooks] = useState<Book[]>([]);
 
-    const id_user = 2;
-    // const { user } = useContext(UserContext);
+    // const id_user = 2;
+    const { user } = useContext(UserContext);
 
     useEffect(() => {
-        fetch(`http://localhost:3000/books?id_user=${id_user}`)
+        fetch(`http://localhost:3000/books?id_user=${user.id_user}`)
             .then(resp => resp.json())
             .then(books => setBooks(books))
             .catch(error => console.log(error))
-    }, [id_user]);
+    }, [user.id_user]);
 
     return (
         <div className="bg-blue-950 page-height overflow-y-scroll w-full">
