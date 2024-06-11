7df7c1f1d910448a35faa5350a3a765492b26b33
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-06-11 15:16:27 +0200

    Refactoriza Menu

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
     return (
         <img
             src="../../public/img/bookstore-logo.png"
-            alt="BookStore logo"
+            alt="BookStore app logo"
             width="40"
         />
     )
