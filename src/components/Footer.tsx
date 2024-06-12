4b9f9d3e4f2bf3667252e4bf4ca5c60424451b41
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-06-12 13:33:46 +0200

    Cambie los links del footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,23 @@
-import { FaFacebook, FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
+import { FaDesktop } from "react-icons/fa"; 
+import { FaGithub } from "react-icons/fa"; 
+import { FaLinkedin } from "react-icons/fa"; 
 
 export default function Footer() {
     return (
-        <ul className="fixed bottom-0 w-full h-[50px] flex items-center justify-center gap-10 font-poppins bg-white">
+        <ul className="fixed bottom-0 w-full h-[50px] px-4 lg:px-10 flex items-center justify-center md:justify-end gap-10 font-poppins bg-white">
             <li>
-                <a href="https://www.facebook.com/" target="_blanc" className="text-blue-950 hover:text-orange-400 transition-all ease-in-out duration-300">
-                    <FaFacebook />
+                <a href="https://www.linkedin.com/in/ekaterina-kushnir-mikhaylova/" target="_blanc" className="text-blue-950 hover:text-orange-400 text-xl md:text-base transition-all ease-in-out duration-300">
+                    <FaLinkedin />
                 </a>
             </li>
             <li>
-                <a href="https://www.instagram.com/" target="_blanc" className="text-blue-950 hover:text-orange-400 transition-all ease-in-out duration-300">
-                    <FaInstagram />
+                <a href="https://github.com/katiaku/bookstore" target="_blanc" className="text-blue-950 hover:text-orange-400 text-xl md:text-base transition-all ease-in-out duration-300">
+                    <FaGithub />
                 </a>
             </li>
             <li>
-                <a href="https://x.com/?lang=en" target="_blanc" className="text-blue-950 hover:text-orange-400 transition-all ease-in-out duration-300">
-                    <FaXTwitter />
+                <a href="https://katiaku-portfolio.vercel.app/" target="_blanc" className="text-blue-950 hover:text-orange-400 text-xl md:text-base transition-all ease-in-out duration-300">
+                    <FaDesktop />
                 </a>
             </li>
         </ul>
