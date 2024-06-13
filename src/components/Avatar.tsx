c61f172c34ed2ecf7d7ca108964966f0bdee9bcf
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-06-13 11:57:21 +0200

    Añade funcionalidad de ir al perfil al hacer click sobre el avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,9 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import { AvatarProps } from "../config/types";
 
 export default function Avatar({ user }: AvatarProps) {
 
+    const navigate = useNavigate();
+
+    function goToProfile () {
+        navigate('/profile');
+    }
+
     return (
-        <div className="flex flex-col items-center mx-2">
+        <div
+        className="flex flex-col items-center mx-2 cursor-pointer"
+        onClick={goToProfile}
+        >
             <div className="border border-white size-10 rounded-full overflow-hidden">
                 <img className="size-full object-cover" src={user.photo || '/img/avatar-placeholder.webp'} alt={`Photo of ${user?.firstName} ${user?.lastName}`} />
             </div>
