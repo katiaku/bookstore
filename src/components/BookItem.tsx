188c973866a90075ac4b4d3a2dbacfd4d62ac50a
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-06-14 13:34:15 +0200

    Añade mejoras de estilo al BookItem

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -1,3 +1,5 @@
+import { RiAmazonFill } from "react-icons/ri"; 
+import { AiFillStar } from "react-icons/ai"; 
 import { RiDeleteBin5Fill, RiEdit2Fill } from "react-icons/ri";
 import { BookItemProps } from "../config/types";
 import { toast } from "react-toastify";
@@ -35,36 +37,51 @@ export default function BookItem(props: BookItemProps) {
         navigate('/edit-book', { state: book });
     }
 
+    // function seeCover {
+
+    // }
+
     return (
         <div className='font-poppins w-full max-w-[400px] md:w-[400px] h-[280px] relative shadow-md bg-slate-200'>
-            <div className='absolute shadow-md left-[15px] -top-[15px] max-h-[280px] overflow-hidden'>
+            <div className='cursor-pointer absolute shadow-md left-[15px] -top-[15px] max-h-[280px] overflow-hidden'>
                 <img
                     src={ book.photo || '../../public/img/no_cover_available.png' }
                     alt={`The cover of ${ book.title } by ${ book.author }` }
                     width="170"
+                    // onClick={seeCover}
                 />
             </div>
 
             <div className='cursor-default bg-slate-200/70 absolute right-0 md:left-[200px] w-1/2 h-full py-[15px] px-[15px] md:pr-[15px] md:pl-0 flex flex-col justify-between items-start'>
-                <div>
-                    <p className='font-bold text-2xl md:text-3xl text-slate-700'>
+                <div className="w-[185px] text-wrap overflow-hidden flex flex-col gap-2">
+                    <p className='font-nunito font-bold text-2xl text-slate-700'>
                         { book.title }
                     </p>
-                    <p className='my-1 text-slate-700'>
-                        { book.author }
+                    <p className='text-xs italic text-slate-700'>
+                        by { book.author }
                     </p>
                     <p className='text-xs italic text-slate-500'>
                         { book.type }
                     </p>
+                    <p className="flex">
+                        <AiFillStar className="text-slate-500 cursor-pointer" />
+                        <AiFillStar className="text-slate-500 cursor-pointer" />
+                        <AiFillStar className="text-slate-500 cursor-pointer" />
+                        <AiFillStar className="text-slate-500 cursor-pointer" />
+                        <AiFillStar className="text-slate-500 cursor-pointer" />
+                    </p>
                 </div>
 
-                <div className='absolute -left-[20%] md:-left-[30px] -bottom-[15px] w-[60px] h-[60px] rounded-full bg-orange-400 text-white flex justify-center items-center shadow-md'>
+                <div className='absolute -left-[20%] md:-left-[30px] -bottom-[15px] w-[60px] h-[60px] rounded-full bg-orange-400 text-white text-sm flex justify-center items-center shadow-md'>
                     <span>
                         { book.price }€
                     </span>
                 </div>
 
                 <div className='self-end flex gap-2'>
+                    <button onClick={ deleteBook }>
+                        <RiAmazonFill className="cursor-pointer text-slate-700 hover:text-orange-400 transition-all ease-in-out duration-300" />
+                    </button>
                     <button onClick={ editBook }>
                         <RiEdit2Fill className="cursor-pointer text-slate-700 hover:text-orange-400 transition-all ease-in-out duration-300" />
                     </button>
